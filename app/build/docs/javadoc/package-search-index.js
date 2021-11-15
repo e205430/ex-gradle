df8bebc0c7a1fdd77719932a0aace534d965b0f0
e205430 <93559099+e205430@users.noreply.github.com>
2021-11-16 01:35:16 +0900

    1st commit

---
+++ app/build/docs/javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"jp.ac.uryukyu.ie.e205430"},{"l":"すべてのパッケージ","u":"allpackages-index.html"}];updateSearchResults();
---
+++ app/build/docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"jp.ac.uryukyu.ie.e205430","l":"App"},{"p":"jp.ac.uryukyu.ie.e205430","l":"Enemy"},{"p":"jp.ac.uryukyu.ie.e205430","l":"Hero"},{"p":"<Unnamed>","l":"Main"}];updateSearchResults();
